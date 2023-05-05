58a16075e6ad1c776353ec08b030fe4578a134fb
Kirill Voronin <voroninkv09@gmail.com>
2023-05-05 09:45:25 +0300

    add changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 //import { contactsReducer, filterReducer } from './reducer';
 import contactsSlice from './contactsSlice';
 import filterSlice from './filterSlice';
