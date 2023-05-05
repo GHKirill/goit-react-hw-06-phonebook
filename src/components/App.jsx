b980a15e41834bd735a24a75f81a07c5e36eec15
Kirill Voronin <voroninkv09@gmail.com>
2023-05-05 09:49:32 +0300

    add change to app.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ export function App() {
     <>
       <div>
         <h1>Phonebook</h1>
-        <ContactForm /> */}
+        <ContactForm />
         <h2>Contacts</h2>
         <Filter />
         <ContactList />
